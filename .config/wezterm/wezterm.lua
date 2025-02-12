e3e7c5f1443d34440dc0a837bfb7d2d5b8113a77
Zaw Min Thant <zmt22@proton.me>
2025-02-12 00:12:16 -0800

    Update WezTerm Config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -21,7 +21,6 @@ config.initial_rows = 30
 config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = "NeverPrompt"
 
-
 -- and finally, return the configuration to wezterm
 return config
 
